a9561c37b18f6f23ffa742411ff07f4005ae19d8
Carlos Castillo <castillo25ca@gmail.com>
2021-03-03 11:00:31 -0800

    updates

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
