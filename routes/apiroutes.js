e515b780097e47d18d9d2df26f2e82f898e19527
Carlos Castillo <castillo25ca@gmail.com>
2021-03-04 16:23:17 -0800

    fixes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,35 +3,20 @@ const router = require('express').Router();
 const db = require('../db/db.json');
 
 router.get("/api/notes", function (req, res) {
-    res.json(db)
-  });
+  res.json(db)
+});
 
 router.post("/api/notes", function (req, res) {
-    
-      const id = Math.floor(Math.random()*1000)
-      const newNote = { title: req.body.title, text: req.body.text, id: id }
-      db.push(newNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db))
-    res.json(db)
+  const id = Math.floor(Math.random() * 1000)
+  const newNote = { title: req.body.title, text: req.body.text, id: id }
+  db.push(newNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(db))
+  res.json(db)
 })
-  router.delete("/api/notes/:id", function (req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
-      if (error) {
-        return console.log(error)
-      }
-      notes = JSON.parse(notes)
-  
-      notes = notes.filter(val => val.id !== noteId)
-  
-      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
-        if (error) {
-          return error
-        }
-        res.json(notes)
-      })
-    })
-  })
-  
+router.delete("/api/notes/:id", function (req, res) {
+  notes.deleteNote(req.params.id)
+  .then(() => res.json({ok: true}))
+  .catch(err => res.status(500).json(err));
+})
+
 module.exports = router;  
