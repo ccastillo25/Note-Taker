6b02af5324ed300921a76b71e02d3954f349d5de
Carlos Castillo <castillo25ca@gmail.com>
2021-03-04 20:53:49 -0800

    test

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -13,11 +13,21 @@ router.post("/api/notes", function (req, res) {
   fs.writeFileSync('db/db.json', JSON.stringify(db))
   res.json(db)
 })
+
 router.delete("/api/notes/:id", function (req, res) {
-  notes.deleteNote(req.params.id)
-  .then(() => res.json({ok: true}))
-  .catch(err => res.status(500).json(err));
-  console.log();
+  // you have db
+  // you need to eliminate the object with the id = req.id,params\
+  db.splice(req.id,params);
+  // and the overwrite the file
+  fs.writeFileSync('db/db.json', JSON.stringify(db))
+  res.json(db)
+  console.log()
 })
+
+// router.delete("/api/notes/:id", function (req, res) {
+//   notes.deleteNote(req.params.id)
+//   .then(() => res.json({ok: true}))
+//   .catch(err => res.status(500).json(err));
+// })
 
 module.exports = router;  
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const htmlRoutes = require('./routes/htmlroutes.js')
 const apiRoutes = require('./routes/apiroutes.js')
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
